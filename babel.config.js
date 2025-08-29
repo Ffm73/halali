c9981f5d907835720d621991d9037728e8966e82
Faisal Almuhaysh <f.mohaish@gmail.com>
2025-08-29 16:12:47 +0300

    Babel plugin verified, caches cleared, scheme set for linking

---
+++ babel.config.js
@@ -0,0 +1,11 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      // ...any other plugins you use,
+      'react-native-reanimated/plugin', // must be last
+    ],
+  };
+};
+
